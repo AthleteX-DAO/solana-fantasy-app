f3657b4328637c75ca6309886e378d5e4eaebb9d
zemse <sohamzemse143@gmail.com>
2020-10-14 22:17:37 +0000

    Add load program test

--- test/global.ts
+++ test/global.ts
@@ -1,9 +1,11 @@
-import { Connection } from '@solana/web3.js';
+import { Connection, Account, PublicKey } from '@solana/web3.js';
 
 declare global {
   namespace NodeJS {
     interface Global {
       connection: Connection;
+      payerAccount: Account;
+      helloWorldPPK: PublicKey; // PPK => Program Public Key
     }
   }
 }
--- test/suites/HelloWorld/LoadProgram.test.ts
+++ test/suites/HelloWorld/LoadProgram.test.ts
@@ -1,18 +1,32 @@
 import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { Account, BpfLoader, BPF_LOADER_DEPRECATED_PROGRAM_ID } from '@solana/web3.js';
+import { strictEqual } from 'assert';
 
 export const LoadProgram = () =>
   describe('Load Program', () => {
     it('loads program to the localnet', async () => {
-      //   const data = readFileSync('../../../build/hello_world.so', {encoding: 'binary'});
-      // const programAccount = new Account();
-      // await BpfLoader.load(
-      //   connection,
-      //   payerAccount,
-      //   programAccount,
-      //   data,
-      //   BPF_LOADER_DEPRECATED_PROGRAM_ID,
-      // );
-      // programId = programAccount.publicKey;
-      // console.log('Program loaded to account', programId.toBase58());
+      const data = readFileSync(resolve(__dirname, '../../../build/hello_world.so'), {
+        encoding: 'hex',
+      });
+      const programAccount = new Account();
+      // const payerAccount = new Account();
+
+      // console.log(data.slice(0, 10));
+
+      const balance = await global.connection.getBalance(global.payerAccount.publicKey);
+      console.log(balance);
+
+      const isLoaded = await BpfLoader.load(
+        global.connection,
+        global.payerAccount,
+        programAccount,
+        Buffer.from(data, 'hex'),
+        BPF_LOADER_DEPRECATED_PROGRAM_ID
+      );
+
+      strictEqual(isLoaded, true, 'load method should return true');
+
+      global.helloWorldPPK = programAccount.publicKey;
     });
   });
