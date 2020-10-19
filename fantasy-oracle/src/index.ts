448eab8fabae9b31768359d046ecda4351baa498
zemse <sohamzemse143@gmail.com>
2020-10-19 18:35:00 +0000

    Add oracle project

---
+++ fantasy-oracle/src/index.ts
@@ -0,0 +1,8 @@
+// import { getPlayerList } from './players-list';
+import { calculateScore } from './calculate-score';
+
+(async () => {
+  console.log(1);
+  // const x = await calculateScore([]);
+  // console.log(x);
+})(); 
