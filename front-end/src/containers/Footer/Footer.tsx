c5dc17858270f78f913e6c13906c5be98d39790c
zemse <sohamzemse143@gmail.com>
2020-10-28 01:41:22 +0000

    Add demo content

--- front-end/src/containers/Footer/Footer.tsx
+++ front-end/src/containers/Footer/Footer.tsx
@@ -10,18 +10,19 @@ export function Footer() {
             <div className="row">
               <div className="col-lg-4 col-md-6">
                 <div className="footer-info">
-                  <h3>Day</h3>
+                  <h3>SFS</h3>
                   <p>
-                    A108 Adam Street <br />
-                    NY 535022, USA
+                    <span style={{ fontFamily: 'monospace' }}>0x2343742837</span>
+                    <br />
+                    Devnet, Solana
                     <br />
                     <br />
-                    <strong>Phone:</strong> +1 5589 55488 55
+                    {/* <strong>Phone:</strong> +1 5589 55488 55
                     <br />
                     <strong>Email:</strong> info@example.com
-                    <br />
+                    <br /> */}
                   </p>
-                  <div className="social-links mt-3">
+                  {/* <div className="social-links mt-3">
                     <a href="#" className="twitter">
                       <i className="bx bxl-twitter"></i>
                     </a>
@@ -37,7 +38,7 @@ export function Footer() {
                     <a href="#" className="linkedin">
                       <i className="bx bxl-linkedin"></i>
                     </a>
-                  </div>
+                  </div> */}
                 </div>
               </div>
 
@@ -45,24 +46,12 @@ export function Footer() {
                 <h4>Useful Links</h4>
                 <ul>
                   <li>
-                    <i className="bx bx-chevron-right"></i> <a href="#">Home</a>
-                  </li>
-                  <li>
-                    <i className="bx bx-chevron-right"></i> <a href="#">About us</a>
-                  </li>
-                  <li>
-                    <i className="bx bx-chevron-right"></i> <a href="#">Services</a>
-                  </li>
-                  <li>
-                    <i className="bx bx-chevron-right"></i> <a href="#">Terms of service</a>
-                  </li>
-                  <li>
-                    <i className="bx bx-chevron-right"></i> <a href="#">Privacy policy</a>
+                    <i className="bx bx-chevron-right"></i> <a href="#">GitHub Repository</a>
                   </li>
                 </ul>
               </div>
 
-              <div className="col-lg-2 col-md-6 footer-links">
+              {/* <div className="col-lg-2 col-md-6 footer-links">
                 <h4>Our Services</h4>
                 <ul>
                   <li>
@@ -81,16 +70,16 @@ export function Footer() {
                     <i className="bx bx-chevron-right"></i> <a href="#">Graphic Design</a>
                   </li>
                 </ul>
-              </div>
+              </div> */}
 
-              <div className="col-lg-4 col-md-6 footer-newsletter">
+              {/* <div className="col-lg-4 col-md-6 footer-newsletter">
                 <h4>Our Newsletter</h4>
                 <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
                 <form action="" method="post">
                   <input type="email" name="email" />
                   <input type="submit" value="Subscribe" />
                 </form>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
@@ -99,7 +88,7 @@ export function Footer() {
           <div className="copyright">
             &copy; Copyright{' '}
             <strong>
-              <span>Day</span>
+              <span>Solana Fantasy Sports</span>
             </strong>
             . All Rights Reserved
           </div>
--- front-end/src/containers/Home/Home.tsx
+++ front-end/src/containers/Home/Home.tsx
@@ -3,15 +3,10 @@ import React from 'react';
 export function Home() {
   return (
     <>
-      {/* <!-- ======= Hero Section ======= --> */}
       <section id="hero" className="d-flex align-items-center">
-        <div
-          className="container position-relative"
-          // data-aos="fade-up"
-          // data-aos-delay="500"
-        >
-          <h1>Welcome to Day</h1>
-          <h2>We are team of talanted designers making websites with Bootstrap</h2>
+        <div className="container position-relative">
+          <h1>Welcome to Solana Fantasy Sports</h1>
+          <h2>Create a league or join a league to start playing</h2>
           <a href="#about" className="btn-get-started scrollto">
             Get Started
           </a>
@@ -20,51 +15,30 @@ export function Home() {
       {/* <!-- End Hero --> */}
 
       <main id="main">
-        {/* <!-- ======= About Section ======= --> */}
         <section id="about" className="about">
           <div className="container">
             <div className="row">
-              <div
-                className="col-lg-6 order-1 order-lg-2"
-                // data-aos="fade-left"
-              >
+              <div className="col-lg-6 order-1 order-lg-2">
                 <img src="assets/img/about.jpg" className="img-fluid" alt="" />
               </div>
-              <div
-                className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content"
-                // data-aos="fade-right"
-              >
-                <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+              <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+                <h3>About the dApp</h3>
                 <p className="font-italic">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua.
+                  This application is built on the Solana, the super fast blockchain.
                 </p>
                 <ul>
                   <li>
-                    <i className="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.
+                    <i className="icofont-check-circled" /> Your keys stays on your system.
                   </li>
                   <li>
-                    <i className="icofont-check-circled"></i> Duis aute irure dolor in reprehenderit
-                    in voluptate velit.
-                  </li>
-                  <li>
-                    <i className="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta
-                    storacalaperda mastiro dolore eu fugiat nulla pariatur.
+                    <i className="icofont-check-circled"></i> Open-sourced
                   </li>
                 </ul>
-                <p>
-                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum
-                </p>
+                <p>This is a demonstration of a Fantasy application on Solana.</p>
               </div>
             </div>
           </div>
         </section>
-        {/* <!-- End About Section --> */}
       </main>
     </>
   );
