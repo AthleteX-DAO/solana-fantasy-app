2142b38122f212ab4dd8170f778b01d3a30dc1be
zemse <sohamzemse143@gmail.com>
2020-10-28 23:18:20 +0000

    Add clojured wallet

---
+++ front-end/src/clojured-wallet.ts
@@ -0,0 +1,34 @@
+import { Account, PublicKey } from '@solana/web3.js';
+import { arrayify, hexlify } from '@ethersproject/bytes';
+
+export type ClojuredWallet = {
+  readonly publicKey: string;
+  readonly privateKey: string;
+  callback(description: string, fn: (programAcc: Account) => any): any;
+};
+
+export function CreateClojuredWallet(secretKey?: string): ClojuredWallet {
+  let _secretKey: Uint8Array | undefined;
+  if (typeof secretKey === 'string') _secretKey = arrayify(secretKey);
+  const programAccount = new Account(_secretKey);
+
+  return {
+    get publicKey() {
+      return programAccount.publicKey.toBase58();
+    },
+    get privateKey() {
+      const _secretKey = this.callback(
+        'Do you want to export private key?',
+        (programAcc: Account) => {
+          return programAcc.secretKey;
+        }
+      );
+      return hexlify(_secretKey);
+    },
+    callback(description: string, fn: (programAcc: Account) => any): any {
+      if (window.confirm(description)) {
+        return fn(programAccount);
+      }
+    },
+  };
+}
