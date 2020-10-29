1bc47ed81f878e6ce19be426b1de2091d961afdb
zemse <sohamzemse143@gmail.com>
2020-10-29 00:11:46 +0000

    Integrate create wallet logic

--- front-end/src/containers/Wallet/CreateWallet.tsx
+++ front-end/src/containers/Wallet/CreateWallet.tsx
@@ -1,30 +1,58 @@
 import React, { FunctionComponent, useState } from 'react';
-import { Card, Form } from 'react-bootstrap';
+import { Alert, Card, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Layout } from '../Layout';
 
+import { CreateClojuredWallet } from '../../clojured-wallet';
+
 export const CreateWallet: FunctionComponent<{}> = (props) => {
-  // let [privateKeyInput, setPrivateKeyInput] = useState<string>('');
+  let [display, setDisplay] = useState<{ message: string; variant: string } | null>(null);
+
+  const createWallet = () => {
+    try {
+      setDisplay({
+        message: 'Please wait creating wallet...',
+        variant: 'warning',
+      });
+      const wallet = CreateClojuredWallet();
+      window.wallet = wallet;
+      setDisplay({
+        message: 'Wallet create successfully!',
+        variant: 'success',
+      });
+
+      Object.entries(window.walletStatusChangeHooks).forEach((entries) => {
+        try {
+          entries[1]();
+        } catch {}
+      });
+    } catch (error) {
+      setDisplay({
+        message: `Error: ${error.message}`,
+        variant: 'danger',
+      });
+    }
+  };
 
   return (
     <Layout heading="Create Wallet">
       <Card style={{ maxWidth: '400px', margin: '0 auto' }}>
         <Card.Body>
-          {/* <Form.Control
-            className="align-items-center"
-            onChange={(event) => setPrivateKeyInput(event.target.value)}
-            value={privateKeyInput}
-            type="text"
-            placeholder="Enter your private key"
-            autoComplete="off"
-            // isInvalid={}
-          /> */}
+          {display !== null ? (
+            <Alert className="my-2" variant={display.variant}>
+              {display.message}
+            </Alert>
+          ) : null}
 
-          <button className="btn mt-4">Create Wallet</button>
+          <button onClick={createWallet} className="btn mt-4">
+            Create Wallet
+          </button>
 
-          <span className="small mt-3 mb-0 display-block">
-            {/* <Link to="/wallet/import">But I already have a wallet.</Link> */}
-          </span>
+          {display?.variant === 'success' ? (
+            <span className="small mt-3 mb-0 display-block">
+              <Link to="/wallet">Go to my wallet</Link>
+            </span>
+          ) : null}
         </Card.Body>
       </Card>
     </Layout>
