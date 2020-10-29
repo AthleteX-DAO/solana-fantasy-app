52c3ab3d2a7d8ad62950170d51a47fd3721738ad
zemse <sohamzemse143@gmail.com>
2020-10-29 00:57:20 +0000

    Add league view page

---
+++ front-end/src/containers/Leagues/LeagueView.tsx
@@ -0,0 +1,50 @@
+import React, { FunctionComponent, useState } from 'react';
+import { Card, Form, InputGroup } from 'react-bootstrap';
+import { RouteComponentProps } from 'react-router-dom';
+import { Layout } from '../Layout';
+
+interface MatchParams {
+  index: string;
+}
+
+export const LeagueView: FunctionComponent<RouteComponentProps<MatchParams>> = (props) => {
+  const [teamNameInput, setTeamNameInput] = useState<string>('');
+  const [feesInput, setFeesInput] = useState<string>('50');
+
+  return (
+    <Layout heading="Create a Team">
+      <p>This is a League with index {props.match.params.index}</p>
+      <Card style={{ maxWidth: '400px', margin: '0 auto' }}>
+        <p className="mt-2 mb-0">Join the league by creating a team</p>
+        <Card.Body>
+          <Form.Control
+            className="align-items-center"
+            onChange={(event) => setTeamNameInput(event.target.value)}
+            value={teamNameInput}
+            type="text"
+            placeholder="Enter Team Name"
+            autoComplete="off"
+            // isInvalid={}
+          />
+
+          <InputGroup className="my-4">
+            <Form.Control
+              className="align-items-center"
+              onChange={(event) => setFeesInput(event.target.value)}
+              value={feesInput}
+              type="text"
+              placeholder="Enter fees"
+              autoComplete="off"
+              // isInvalid={}
+            />
+            <InputGroup.Append>
+              <InputGroup.Text id="basic-addon2">SOL</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+
+          <button className="btn mt-4">Join</button>
+        </Card.Body>
+      </Card>
+    </Layout>
+  );
+};
--- front-end/src/containers/Leagues/Router.tsx
+++ front-end/src/containers/Leagues/Router.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { LeagueList } from './LeagueList';
+import { LeagueView } from './LeagueView';
 
 export const LeaguesRouter: FunctionComponent<{}> = (props) => {
   return (
     <Switch>
       <Route path="/leagues" exact component={LeagueList} />
+      <Route path="/leagues/:index" exact component={LeagueView} />
     </Switch>
   );
 };
