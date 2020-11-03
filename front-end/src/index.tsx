940655b4100ba847f2ec6103190ddd59e1a04ff6
zemse <sohamzemse143@gmail.com>
2020-11-03 21:23:37 +0000

    Implement root caching in global var

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -9,6 +9,7 @@ import { Connection, PublicKey } from '@solana/web3.js';
 import { SFS } from './sdk/sfs';
 import { CreateClojuredWallet } from './clojured-wallet';
 import { Buffer } from 'buffer';
+import { Root } from './sdk/state';
 
 window.Buffer = Buffer;
 window.sfsProgramId = new PublicKey('yX8ip4PTAZs261A7s5ZaZjzMYbPjEeRjWYArDA7sZjf');
@@ -17,6 +18,29 @@ window.connection = new Connection('https://devnet.solana.com', 'recent');
 const bankPromise = PublicKey.findProgramAddress([Buffer.from([0])], window.sfsProgramId);
 window.sfsSDK = async () =>
   new SFS(window.connection, window.sfsRoot, window.sfsProgramId, (await bankPromise)[0]);
+
+let root_cache: { root: Root; expiry: number } | null = null;
+let root_new_fetching_promise: Promise<Root> | null = null;
+window.getCachedRootInfo = async (forceUpdate?: boolean): Promise<Root> => {
+  const sfs = await window.sfsSDK();
+  if (root_cache === null || forceUpdate || Date.now() > root_cache.expiry) {
+    if (root_new_fetching_promise === null) {
+      root_new_fetching_promise = sfs.getRootInfo().then((root) => {
+        root_cache = { root, expiry: Date.now() + 10000 };
+        root_new_fetching_promise = null;
+        return root;
+      });
+    }
+
+    if (root_cache === null || forceUpdate) {
+      const prom = await root_new_fetching_promise;
+      // @ts-ignore When root_new_fetching_promise is resolved, it sets root_cache
+      return root_cache.root;
+    }
+  }
+
+  return root_cache.root;
+};
 
 if (process.env.NODE_ENV !== 'production') {
   window.wallet = CreateClojuredWallet(
