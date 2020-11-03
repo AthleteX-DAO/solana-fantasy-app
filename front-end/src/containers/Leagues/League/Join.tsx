2583c745a64b1d56ac2c5fb40741628ac26d0cdc
zemse <sohamzemse143@gmail.com>
2020-11-03 22:29:15 +0000

    Implement join league

--- front-end/src/containers/Leagues/League/Join.tsx
+++ front-end/src/containers/Leagues/League/Join.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Card, Form, InputGroup } from 'react-bootstrap';
 import { RouteComponentProps, useHistory } from 'react-router-dom';
 import { MatchParams } from './Forwarder';
@@ -8,15 +8,42 @@ export const JoinLeague: FunctionComponent<RouteComponentProps<MatchParams>> = (
   const leagueIndex = +props.match.params.index;
   const history = useHistory();
 
+  const [spinner, setSpinner] = useState<boolean>(false);
   const [teamNameInput, setTeamNameInput] = useState<string>('');
   const [feesInput, setFeesInput] = useState<string>('50');
+  const [leagueName, setLeagueName] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const root = await window.getCachedRootInfo();
+      setFeesInput(String(root.leagues[leagueIndex].bid.toNumber() / 10 ** 9));
+      setLeagueName(root.leagues[leagueIndex].name);
+    })().catch(console.error);
+  }, []);
+
+  const joinLeague = async () => {
+    if (!window.wallet) {
+      throw new Error('Wallet not loaded');
+    }
+    const sdk = await window.sfsSDK();
+
+    const resp = await window.wallet.callback('Sign on Create League transaction?', async (acc) => {
+      await sdk.joinLeague(acc, leagueIndex);
+    });
+    console.log({ resp });
+  };
 
   return (
     <Layout removeTopMargin heading="Join League">
       <Card style={{ maxWidth: '400px', margin: '0 auto' }}>
-        <p className="mt-2 mb-0">Join the league {leagueIndex} by creating a team</p>
+        <p className="mt-2 mb-0">
+          Join the{' '}
+          <strong>{leagueName ? <>{leagueName} league</> : <>leage {leagueIndex}</>}</strong> by
+          creating a team
+        </p>
         <Card.Body>
           <Form.Control
+            disabled={spinner}
             className="align-items-center"
             onChange={(event) => setTeamNameInput(event.target.value)}
             value={teamNameInput}
@@ -28,6 +55,7 @@ export const JoinLeague: FunctionComponent<RouteComponentProps<MatchParams>> = (
 
           <InputGroup className="my-4">
             <Form.Control
+              disabled={spinner}
               className="align-items-center"
               onChange={(event) => setFeesInput(event.target.value)}
               value={feesInput}
@@ -42,10 +70,23 @@ export const JoinLeague: FunctionComponent<RouteComponentProps<MatchParams>> = (
           </InputGroup>
 
           <button
-            onClick={() => history.push(`/leagues/${leagueIndex}/draft-selection`)}
+            disabled={spinner}
+            onClick={() => {
+              setSpinner(true);
+              joinLeague()
+                .then(() => {
+                  setSpinner(false);
+                  history.push(`/leagues/${leagueIndex}/draft-selection`);
+                })
+                .catch((err) => {
+                  alert('Error:' + err?.message ?? err);
+                  console.log(err);
+                  setSpinner(false);
+                });
+            }}
             className="btn mt-4"
           >
-            Join by paying {feesInput} SOL
+            {spinner ? 'Joining...' : <>Join by paying {feesInput} SOL</>}
           </button>
         </Card.Body>
       </Card>
