ba0c374ad6a7b859f0621f90760373f4910c43f1
zemse <sohamzemse143@gmail.com>
2020-11-03 18:47:06 +0000

    Fix sfs bank issue

--- fantasy-oracle/scripts/commons.ts
+++ fantasy-oracle/scripts/commons.ts
@@ -13,11 +13,12 @@ export const wallet = new Account(arrayify(privateKey));
 console.log('Wallet loaded', wallet.publicKey.toBase58());
 
 // localnet program
-export const programId = new PublicKey('9yfdNX1GJGpAM4cVF9DPWhDbpCFTnqTpLFqRVPtrjgvo');
-export const rootPublicKey = new PublicKey('2qX95gJwuXfDK3inyV6D8tEyo71q9SCfe7gNs6kesmk9');
+export const programId = new PublicKey('FHmhZ4znQyoLVQnqxT3LMNn9RACPyvmAuqJ4xqo3133W');
+export const rootPublicKey = new PublicKey('3WSGdDabgaPZfG2ZrqCBNv1FoHV68bvv7T4sWCKwYTxm');
 
 // devnet program
 // export const programId = new PublicKey('yX8ip4PTAZs261A7s5ZaZjzMYbPjEeRjWYArDA7sZjf');
 // export const rootPublicKey = new PublicKey('6P4JL1Hc9d1pKnWHeV99Bq3BZRSLudCKM9fi1cCnp3sj');
-
-export const sfs = new SFS(connection, rootPublicKey, programId, wallet.publicKey);
+const bankPromise = PublicKey.findProgramAddress([Buffer.from([0])], programId);
+export const sfsFn = async () =>
+  new SFS(connection, rootPublicKey, programId, (await bankPromise)[0]);
--- fantasy-oracle/scripts/create-league.ts
+++ fantasy-oracle/scripts/create-league.ts
@@ -1,16 +1,8 @@
 import { PublicKey } from '@solana/web3.js';
-import { connection, wallet, sfs } from './commons';
+import { connection, wallet, sfsFn } from './commons';
 
 (async () => {
-  const rootBefore = await sfs.getRootInfo();
-  console.log('currentWeek before', rootBefore.currentWeek);
-
-  console.log('waiting for 3 sec');
-  await new Promise((res) => setTimeout(res, 3000));
-
-  console.log('incrementing week');
+  const sfs = await sfsFn();
+  console.log('creating league');
   await sfs.createLeague(wallet, 'hi', 1 * 10 ** 9, 12);
-
-  const rootAfter = await sfs.getRootInfo();
-  console.log('currentWeek after', rootAfter.currentWeek);
 })().catch(console.error);
