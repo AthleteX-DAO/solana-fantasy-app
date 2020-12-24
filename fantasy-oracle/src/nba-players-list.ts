810eb5e5db40c6fae663ccda5ee09c5ca009168f
Kevin Kamto <kamtokevin@gmail.com>
2020-12-24 13:08:39 -0500

    feat: finished nba-players-list

--- fantasy-oracle/src/nba-players-list.ts
+++ fantasy-oracle/src/nba-players-list.ts
@@ -1,8 +1,21 @@
 interface NBAPlayerRaw {
     FantasyPlayerKey: string,
-    PlayerID: number; // 19801,
-    Name: string; // 'Josh Allen',
-    Team: string; // 'BUF',
+    PlayerID: number; 
+    Position: string;
+    Name: string; 
+    Team: string; 
+    Games: number;
+    FantasyPoints: number;
+    FieldGoalsMade: number;
+    FreeThrowsMade: number;
+    TwoPointersMade: number;
+    ThreePointersMade: number;
+    Rebounds: number;
+    Assists: number;
+    Points: number;
+    PersonalFouls: number;
+    LineupStatus: number;
+    TrueShootingAttempts: number;
 }
 
 
@@ -12,17 +25,16 @@ export interface NBAPlayer {
     PlayerID: number,
     Name: string,
     Position: string,
-    FG: string
 }
 
-export async function getPlayerList(): Promise<NBAPlayer[]> {
+export async function getNBAPlayerList(): Promise<NBAPlayer[]> {
 
-    const res: AxiosResponse<NBAPlayerRaw> = await axios.get(
-        'https://api.sportsdata.io/v3/'
+    const res: AxiosResponse<NBAPlayerRaw[]> = await axios.get(
+        'https://api.sportsdata.io/v3/nba/projections/json/PlayerSeasonProjectionStats/2020?key=b00ef84629c94abc9e4acfa66a920ac5'
     );
 
-    return res.data.map((player: JSON) => {
-        const {  } = player;
-        return {  };
+    return res.data.map((player) => {
+        const { PlayerID, Name, Position } = player;
+        return { PlayerID, Name, Position };
     });
 }
