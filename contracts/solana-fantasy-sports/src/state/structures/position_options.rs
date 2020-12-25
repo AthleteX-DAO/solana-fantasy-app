f762425758999c5dd50296877ae1c9bccf13a0c1
Matthew Klengson <mklengs@clemson.edu>
2020-12-24 22:11:00 -0500

    Bug Fixes and prep work
    Updated the sdk state to mirror changes made in the program (positions + constants)
    
    Fixed bugs with the position options impl and added it  to the mod

--- contracts/solana-fantasy-sports/src/state/structures/position_options.rs
+++ contracts/solana-fantasy-sports/src/state/structures/position_options.rs
@@ -1,6 +1,10 @@
 //Structure implemented by each league to determine the number of players / position to use
 use crate::state::*;
 use std::cell::RefCell;
+use solana_program::{
+    program_error::ProgramError
+};
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 
 pub struct PositionOptions<'a>{
     data: &'a RefCell<&'a mut [u8]>,
@@ -12,13 +16,18 @@ pub struct PositionOptions<'a>{
     Get and set the number of players / position available to select for each team in the league
 */
 impl <'a> PositionOptions<'a>{
-    pub const LEN = NUM_POSITIONS-1; //Ignore type of position: Uninitialized == 6
+    pub const LEN:usize = NUM_POSITIONS as usize -1; //Ignore type of position: Uninitialized == 6
 
     //Each slice represents a position
     fn slice<'b>(
         &self,
         data: &'b mut [u8],
     ) -> (
+        &'b mut [u8; 1],
+        &'b mut [u8; 1],
+        &'b mut [u8; 1],
+        &'b mut [u8; 1],
+        &'b mut [u8; 1],
         &'b mut [u8; 1],
         &'b mut [u8; 1],
         &'b mut [u8; 1],
@@ -33,16 +42,34 @@ impl <'a> PositionOptions<'a>{
             1,
             1,
             1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1
         ]
     }
-
     // May have to decrement position so that it matches with the position structure and ignores unitialized
 
-    pub fn get_number_by_position(&self,value: Position) -> u8{
-        self.slice(&mut self.data.borrow_mut()).value[0]
+    //Value is position
+    pub fn get_number_by_position(&self,value: usize) -> u8{ 
+        let data = &mut self.data.borrow_mut();
+        array_mut_ref![data,self.offset+value,1][0]
     }
-    pub fn set_number_by_position(pos: Position, value: u8){
-        self.slice(&mut self.data.borrow_mut()).pos[0] = value;
+    /*
+    pub fn set_number_by_position(&self,pos: Position, value: u8){
+        self.slice(&mut self.data.borrow_mut())[pos] = value;
+    }*/
+    pub fn set(&self, values: [u8;11]){
+        let data = &mut self.data.borrow_mut();
+        array_mut_ref![data, self.offset, PositionOptions::LEN].copy_from_slice(&values);
+    }
+
+    pub fn new(data: &'a RefCell<&'a mut [u8]>, offset: usize) -> Result<PositionOptions,ProgramError>{
+        if data.borrow().len() < Self::LEN + offset {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        Ok(PositionOptions{data,offset})
     }
 }
