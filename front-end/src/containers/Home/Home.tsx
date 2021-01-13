ae542b08d152b63c8a97df103452ade23ac85d9c
Kevin Kamto <kamtokevin@gmail.com>
2021-01-12 23:09:49 -0500

    tweak: style + create my account

--- front-end/src/containers/Home/Home.tsx
+++ front-end/src/containers/Home/Home.tsx
@@ -8,16 +8,25 @@ export function Home() {
         <div className="container position-relative">
           <h1>Athlete.Equity</h1>
           <h2>Invest in the player performance of athletes</h2>
-          <Link
-            to={!!window.wallet ? '/leagues/create' : '/wallet/import'}
-            className="btn-get-started scrollto"
-          >
-            Let's Do This
-          </Link>
-        </div>
+              <div className="bx-align-right">
+              <Link
+                to={!!window.wallet ? '/leagues/create' : '/wallet/import'}
+                className="btn-get-started scrollto"
+              >
+                Create my Account
+              </Link>
+              </div>
+          </div>
+
       </section>
       {/* <!-- End Hero --> */}
 
+      <section id="hero" className="d-flex align-itmes-center news-cycle">
+        
+      </section>
+      <section id="hero">
+
+      </section>
       <main id="main"></main>
     </>
   );
